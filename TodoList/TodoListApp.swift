b390e766c47d1b40ab213f3bc8721a49ad5bb245
Aman <amanbind007@gmail.com>
2023-09-01 15:59:32 +0530

    initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Aman Bind on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
