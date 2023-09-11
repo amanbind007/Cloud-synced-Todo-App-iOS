c6ad67ae4fdcb052558c5f1d3b79e26eaf0a74d9
Aman <amanbind007@gmail.com>
2023-09-11 21:25:56 +0530

    Qurying Data from Firestore and Display it on the Screen

--- TodoList/Views/TodoListView.swift
+++ TodoList/Views/TodoListView.swift
@@ -6,23 +6,32 @@
 //
 
 import SwiftUI
+import FirebaseFirestoreSwift
 
 struct TodoListView: View {
     
     private let userId: String
     
     @StateObject var  todoListViewModel = TodoListViewViewModel()
+    @FirestoreQuery var items : [TodoItem]
     
     init(userId: String) {
         self.userId = userId
         
         UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont(name: "Pacifico", size: 40)!, .strokeColor: Color.pink, .foregroundColor: UIColor.systemPink]
+        
+        self._items = FirestoreQuery(collectionPath: "User/\(userId)/Todos")
     }
     
     var body: some View {
         NavigationView {
             
             VStack {
+                
+                List(items) { item in
+                    Text(item.title)
+                }
+                .listItemTint(.pink)
                 
                 
                 
@@ -50,6 +59,6 @@ struct TodoListView: View {
 
 struct TodoListView_Previews: PreviewProvider {
     static var previews: some View {
-        TodoListView(userId: "")
+        TodoListView(userId: "Pgytl8s6vaM4ZqunwxZ0QS03WU33")
     }
 }
